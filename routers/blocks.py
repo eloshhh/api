2c8ba7744e0231230404ee481c102376ad42eef8
ela dogan <elxdogan@gmail.com>
2025-08-22 15:45:38 +0300

    Update blocks.py

--- routers/blocks.py
+++ routers/blocks.py
@@ -1,1 +1,28 @@
+from fastapi import APIRouter
+from api.models.blocks import Block
+from api.services import blocks as block_service
 
+router = APIRouter()
+
+@router.get("/blocks")
+def get_blocks():
+    return block_service.get_all_blocks()
+
+@router.post("/blocks")
+def add_block(block: Block):
+    result = block_service.add_block(block.category_id, block.title, block.content)
+    if result:
+        return result
+    return {"error": "Geçersiz kategori id"}
+
+@router.put("/blocks/{block_id}")
+def update_block(block_id: int, block: Block):
+    result = block_service.update_block(block_id, block.category_id, block.title, block.content)
+    if result:
+        return result
+    return {"error": f"Block {block_id} not found"}
+
+@router.delete("/blocks/{block_id}")
+def delete_block(block_id: int):
+    success = block_service.delete_block(block_id)
+    return {"message": "Deleted"} if success else {"error": f"Block {block_id} not found"}
